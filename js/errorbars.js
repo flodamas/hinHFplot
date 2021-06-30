a1297536b318623e30cc6a37625e987c7611502c
boundino <boundino@gmail.com>
2021-06-29 23:40:56 -0400

    vline for double ratio

--- js/errorbars.js
+++ js/errorbars.js
@@ -70,7 +70,7 @@ var drawaxisgrid = function()
 
     var obs = document.getElementById('observable').value;
     var vy = 0;
-    if(obs == "RAA" || obs == "RpA" || obs == "RpARAA") vy = 1;
+    if(obs == "RAA" || obs == "RpA" || obs == "RpARAA" || obs == "DoubleRatio") vy = 1;
     else if(obs == "LcToD0") vy = -10;
 
     if(vy > ymin && vy < ymax)
