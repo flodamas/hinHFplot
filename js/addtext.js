68453c7fcf3f8dcfad5a61bde1e4b54e7dffcdeb
boundino <boundino@gmail.com>
2023-04-05 15:44:10 -0400

    save value for bold/italic to fix bug when resize

--- js/addtext.js
+++ js/addtext.js
@@ -2,45 +2,60 @@ function addtext()
 {
     var textfarm = document.getElementById("textfarm");
     var ntext = document.getElementById("btnaddtext").value;
+    var iname = "text" + ntext;
     var itextgroup = document.createElement("div");
-    itextgroup.id = "text" + ntext;
+    itextgroup.id = iname;
     textfarm.appendChild(itextgroup);
-    var textsvgid = "text" + ntext + "svg";
+    var textsvgid = iname + "svg";
 
     var iminus = document.createElement("i");
     iminus.setAttribute("class", "fa-solid fa-trash-can");
     iminus.setAttribute("style", "margin: 0 0.3rem; cursor: pointer;");
-    iminus.setAttribute('onclick', 'document.getElementById("'+itextgroup.id+'").remove(); document.getElementById("'+textsvgid+'").remove();');
+    iminus.setAttribute('onclick', 'document.getElementById("'+iname+'").remove(); document.getElementById("'+iname+'svg").remove();');
     itextgroup.appendChild(iminus);
 
     var iinput = document.createElement("input");
     iinput.setAttribute('type', 'text');
-    iinput.id = "text" + ntext + "content";
+    iinput.id = iname + "content";
     iinput.value = "eg. D<sup>0</sup>, J/&psi;, p<sub>T</sub>";
-    iinput.setAttribute('onkeyup', 'changecontent("'+textsvgid+'", this.value)');
+    iinput.setAttribute('onkeyup', 'changecontent("'+iname+'svg", this.value)');
     itextgroup.appendChild(iinput);
+
+    var iboldval = document.createElement("input");
+    iboldval.setAttribute('type', 'number');
+    iboldval.id = iname + "tbold";
+    iboldval.value = 0;
+    iboldval.style.display = "none";
+    itextgroup.appendChild(iboldval);
 
     var ibold = document.createElement("i");
     ibold.setAttribute("class", "fa-solid fa-bold");
     ibold.setAttribute("style", "margin: 0 0.3rem; cursor: pointer;");
-    ibold.setAttribute('onclick', 'switchbold("'+textsvgid+'")');
+    ibold.setAttribute('onclick', 'switchbold("'+iname+'"); changebold("'+iname+'");');
     itextgroup.appendChild(ibold);
 
+    var iitalicval = document.createElement("input");
+    iitalicval.setAttribute('type', 'number');
+    iitalicval.id = iname + "titalic";
+    iitalicval.value = 0;
+    iitalicval.style.display = "none";
+    itextgroup.appendChild(iitalicval);
+    
     var iitalic = document.createElement("i");
     iitalic.setAttribute("class", "fa-solid fa-italic");
     iitalic.setAttribute("style", "cursor: pointer;");
     iitalic.setAttribute("style", "margin: 0 0.3rem 0 0; cursor: pointer;");
-    iitalic.setAttribute('onclick', 'switchitalic("'+textsvgid+'")');
+    iitalic.setAttribute('onclick', 'switchitalic("'+iname+'"); changeitalic("'+iname+'");');
     itextgroup.appendChild(iitalic);
 
     var itsize = document.createElement("input");
     itsize.setAttribute('type', 'number');
     itsize.setAttribute('min', '0');
-    itsize.setAttribute('onchange', 'changetsize("'+textsvgid+'", this.value)');
+    itsize.setAttribute('onchange', 'changetsize("'+iname+'svg", this.value)');
     itsize.setAttribute('style', 'width: 4vw;');
     itsize.setAttribute('value', '50');
     itsize.setAttribute('step', '5');
-    itsize.id = "text" + ntext + "tsize";
+    itsize.id = iname + "tsize";
     itextgroup.appendChild(itsize);
 
     var ifa2 = document.createElement("i");
@@ -54,8 +69,8 @@ function addtext()
     itx.setAttribute('min', '10');
     itx.setAttribute('max', '99');
     itx.setAttribute('value', '15');
-    itx.setAttribute('oninput', 'changetxx("'+textsvgid+'", this.value)');
-    itx.id = "text" + ntext + "itx";
+    itx.setAttribute('oninput', 'changetxx("'+iname+'svg", this.value)');
+    itx.id = iname + "itx";
     itextgroup.appendChild(itx);
 
     var ifa3 = document.createElement("i");
@@ -69,33 +84,31 @@ function addtext()
     ity.setAttribute('min', '10');
     ity.setAttribute('max', '99');
     ity.setAttribute('value', '15');
-    ity.setAttribute('oninput', 'changetyy("'+textsvgid+'", this.value)');
-    ity.id = "text" + ntext + "ity";
+    ity.setAttribute('oninput', 'changetyy("'+iname+'svg", this.value)');
+    ity.id = iname + "ity";
     itextgroup.appendChild(ity);
 
-    addtexttosvg(textsvgid, itx.value, ity.value, itsize.value, iinput.value);
+    addtexttosvg(iname);
     document.getElementById("btnaddtext").value = parseInt(ntext) + 1;
 }
 
-function addtexttosvg(name, xx, yy, tsize, content)
-{
-    var ttext = svg.append("text")
-        .attr("x", getx0(xx))
-        .attr("y", gety0(yy))
-        .attr("id", name)
-        .style("font-size", gettsize(tsize)+"em")
-        .style("text-anchor", "start");
-
-    changecontent(name, content);
-}
-
-function drawtext(name)
+function addtexttosvg(name)
 {
     var xx = document.getElementById(name + "itx").value,
         yy = document.getElementById(name + "ity").value,
         tsize = document.getElementById(name + "tsize").value,
         content = document.getElementById(name + "content").value;
-    addtexttosvg(name + "svg", xx, yy, tsize, content);
+
+    var ttext = svg.append("text")
+        .attr("x", getx0(xx))
+        .attr("y", gety0(yy))
+        .attr("id", name+"svg")
+        .style("text-anchor", "start");
+
+    changecontent(name+"svg", content);
+    changetsize(name+"svg", tsize);
+    changebold(name);
+    changeitalic(name);
 }
 
 function drawalltext()
@@ -104,45 +117,53 @@ function drawalltext()
     for (var i = 0; i < children.length; i++)
     {
         var itext = children[i];
-        drawtext(itext.id);
+        addtexttosvg(itext.id);
     }
 }
 
-function changecontent(name, content)
+function changecontent(tname, content)
 {
-    document.getElementById(name).innerHTML = '';
+    document.getElementById(tname).innerHTML = '';
     var contents = parsescript(content);
     for(var p in contents)
     {
-        svg.select('#' + name).append('tspan')
+        svg.select('#' + tname).append('tspan')
             .attr("class", contents[p].cl)
             .attr("dominant-baseline", "middle")
             .text(decodehtml(contents[p].content));
     }
-    settsuptsub(name);
+    settsuptsub(tname);
 }
 
-function changetsize(name, tsize)
+function changetsize(tname, tsize)
 {
-    svg.select('#' + name).style("font-size", gettsize(tsize)+"em");
+    svg.select('#' + tname).style("font-size", gettsize(tsize)+"em");
 }
 
-function changetxx(name, xx)
+function changetxx(tname, xx)
 {
-    svg.select('#' + name).attr("x", getx0(xx));
+    svg.select('#' + tname).attr("x", getx0(xx));
 }
 
-function changetyy(name, yy)
+function changetyy(tname, yy)
 {
-    svg.select('#' + name).attr("y", gety0(yy));
+    svg.select('#' + tname).attr("y", gety0(yy));
 }
 
 function switchbold(name)
 {
-    document.getElementById(name).style.fontWeight = (document.getElementById(name).style.fontWeight=="bold"?"normal":"bold");
+    document.getElementById(name+"tbold").value = 1 - document.getElementById(name+"tbold").value;
+}
+function changebold(name)
+{
+    document.getElementById(name+"svg").style.fontWeight = document.getElementById(name+"tbold").value>0?"bold":"normal";
 }
 
 function switchitalic(name)
 {
-    document.getElementById(name).style.fontStyle = (document.getElementById(name).style.fontStyle=="italic"?"normal":"italic");
+    document.getElementById(name+"titalic").value = 1 - document.getElementById(name+"titalic").value;
+}
+function changeitalic(name)
+{
+    document.getElementById(name+"svg").style.fontStyle = document.getElementById(name+"titalic").value>0?"italic":"normal";
 }
